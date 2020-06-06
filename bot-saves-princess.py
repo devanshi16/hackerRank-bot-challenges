524d25b67613d5c811563bf0d03d098c5646bb91
Devanshi Malaviya <devanshimalaviya1@gmail.com>
2020-06-06 17:08:36 -0400

    initial commit

--- bot-saves-princess.py
+++ bot-saves-princess.py
@@ -1,3 +1,6 @@
+#Princess Peach is trapped in one of the four corners of a square grid. 
+# You are in the center of the grid and can move one step at a time in any of the four directions. 
+# Can you rescue the princess?
 def nextMove(m,grid):
     p=[0,0]
     m=[s//2,s//2]
